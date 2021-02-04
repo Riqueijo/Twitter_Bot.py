c3a3c4646fbcd991981b2e816b5cf646f9713b01
Henrique Daguerre <78436064+Riqueijo@users.noreply.github.com>
2021-02-04 17:33:00 -0300

    Update main_v1.0.py

--- main_v1.0.py
+++ main_v1.0.py
@@ -15,17 +15,17 @@ def banco_armazenar(historico):
     arquivo.write(f'{historico} \n')
 
 
-consumer_key = 'D3vHIJXY3XSgfGm26UM7PaLV6'
-consumer_secret = 'BMVoiHCUi6HkQadtw0XSFNb6ZvZB74mKsMHtOkQh2uq4izVxes'
-access_key = '1203454489077993472-oSaatY1f5rjqPcYQigmiqR8ObF5mF1'
-access_secret = 'mPDDp44YLLoLASzhqCN3diGeaXY0BdIS1Rem3E4WMGRsF'
+consumer_key = ''
+consumer_secret = ''
+access_key = ''
+access_secret = ''
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret) #login tweepy
 auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
 ############################################### escolhe a foto
-lista = os.listdir('C:/Users/dague/PycharmProject/twitter_bot/bot 2/fotos') #cria a lista de documentos
+lista = os.listdir('/fotos') #cria a lista de documentos
 numero_de_docs = len(lista) #conta a lista
 intervalo = randint(0, numero_de_docs -1) #escolhe um elemento da lista
 
